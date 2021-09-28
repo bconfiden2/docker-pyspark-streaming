c38cc0fe712c1941f7674d3bde9d55a759aa7fda
bconfiden2 <bconfiden2@naver.com>
2021-09-28 10:48:40 +0900

    MODIFY : automatically delete <none> <none> images
    빌드시 태그 중복되어서 dangling 된 이미지 자동으로 삭제

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,7 @@
 sudo docker build --force-rm --tag spark-base:3.0.3 ./spark
 sudo docker build --force-rm --tag spark-master:3.0.3 ./master
 sudo docker build --force-rm --tag spark-worker:3.0.3 ./worker
+sudo docker image prune -f
 
 sudo apt-get update
 sudo apt-get install -y docker-compose
