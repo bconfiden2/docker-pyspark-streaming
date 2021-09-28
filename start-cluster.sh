3604252c659b99b3fe7a04f00c00ac45c2b0828a
bconfiden2 <bconfiden2@naver.com>
2021-09-28 16:23:45 +0900

    modify : do not install docker-compose in script
    스크립트에서 매번 docker-compose 다운로드 받지 않음
    사용자가 미리 설치해놓을것

--- start-cluster.sh
+++ start-cluster.sh
@@ -3,9 +3,6 @@ sudo docker build --force-rm --tag spark-master:3.0.3 ./master
 sudo docker build --force-rm --tag spark-worker:3.0.3 ./worker
 sudo docker image prune -f
 
-sudo apt-get update
-sudo apt-get install -y docker-compose
-
 sudo docker-compose up -d
 
 sudo docker exec -d spark-master /opt/spark-3.0.3-bin-hadoop2.7/bin/pyspark --master spark://spark-master:7077
