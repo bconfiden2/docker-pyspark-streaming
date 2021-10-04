a513c4546f34fc457d05d30b5ce4b11dae05edb4
bconfiden2 <bconfiden2@naver.com>
2021-10-04 15:06:32 +0900

    MODIFY : Basic Operation 에서 사용하는 데이터 생성 방식 변경
    기본 csv 파일인 init.csv 파일 제거
    generator 코드 변경 - 파라미터로 2 넣어주기

--- generator.py
+++ generator.py
@@ -4,20 +4,46 @@ import time
 import os
 import signal
 
-def handler(sig, frame):
+def handler2(sig, frame):
     os.system('rm data/Structured_Streaming/data_*')
     sys.exit()
 
-signal.signal(signal.SIGINT, handler)
-idx = 1
-while True:
-    with open(f"data/Structured_Streaming/data_"+str(idx).zfill(4)+".csv", "w") as f:
-        for i in range(50):
-            qty = str(random.randint(1,10))
-            price = str(round(random.uniform(1.0, 21.0), 2))
-            buy = "buy" if random.randint(1,10) <= 8 else "sell"
-            f.write("-,1,1," + ",".join((qty, price, buy)) + "\n")
-    time.sleep(5)
-    idx += 1
+def basic_operations():
+    signal.signal(signal.SIGINT, handler2)
+    idx = 1
+    while True:
+        with open(f"data/Structured_Streaming/data_"+str(idx).zfill(4)+".csv", "w") as f:
+            for i in range(50):
+                qty = str(random.randint(1,10))
+                price = str(round(random.uniform(1.0, 21.0), 2))
+                buy = "buy" if random.randint(1,10) <= 8 else "sell"
+                f.write("-,1,1," + ",".join((qty, price, buy)) + "\n")
+        time.sleep(5)
+        idx += 1
+    signal.pause()
 
-signal.pause()
+
+length3 = 1
+def handler3(sig, frame):
+    global length3
+    for idx in range(1, length3+1):
+        os.system(f"mv data/Structured_Streaming/twitter_{str(idx).zfill(2)} data/Structured_Streaming/Twitter/twitter_{str(idx).zfill(2)}")
+    sys.exit()
+
+def window():
+    global length3
+    signal.signal(signal.SIGINT, handler3)
+    for idx in range(1, 31):
+        os.system(f"mv data/Structured_Streaming/Twitter/twitter_{str(idx).zfill(2)} data/Structured_Streaming/twitter_{str(idx).zfill(2)}")
+        length3 = idx
+        time.sleep(5)
+    signal.pause()
+
+
+if not len(sys.argv) > 1:
+    print("not enough arguments, usecase: 'python3 generator.py [NUM]'")
+    sys.exit()
+if int(sys.argv[1]) == 2:
+    basic_operations()
+elif int(sys.argv[1]) == 3:
+    window()
